e3020f3861140834ea22514c98c863b704170eb8
alekseypetrenko <menefrego5@gmail.com>
2024-01-26 18:08:12 +0200

    іендув ьщвфдфstyled modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -33,8 +33,8 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption="Close">
-        <h2>Invalid input</h2>
-        <p>Ooops...you forgot to add a value</p>
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid input</h2>
+        <p className="text-stone-600 mb-4">Ooops...you forgot to add a value</p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
